a5bd2a8449ddbec01c139e0558fe6a70146804ad
IE-MangChi <hsm983278@gmail.com>
2023-12-05 18:51:31 +0900

    fix: 제목, 내용, 작성자만 생성자로 받도록 변경

--- Lets_Do_It/practice/src/main/java/study/practice/domain/board/Board.java
+++ Lets_Do_It/practice/src/main/java/study/practice/domain/board/Board.java
@@ -1,15 +1,21 @@
 package study.practice.domain.board;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Board {
 
     private Long id; // 글 식별 id
+
     private String title; // 글 제목
     private String content; // 글 내용
     private String writer; // 작성자
     private Long hit; // 조회수
+
+    public Board(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.hit = 0L;
+    }
 }
