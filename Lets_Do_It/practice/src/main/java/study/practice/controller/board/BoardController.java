6065d5d9838de8f008efc9ca60aba94c822c71bd
IE-MangChi <hsm983278@gmail.com>
2023-12-05 22:02:27 +0900

    feat: 게시판 컨트롤러 기능 구현
    -board: 상세 게시물 보기
    -addBoard: 게시물 등록폼 이동
    -createBoard: 게시물 등록 하기
    -editForm: 게시물 수정폼 이동
    -editboard: 게시물 수정하기

--- Lets_Do_It/practice/src/main/java/study/practice/controller/board/BoardController.java
+++ Lets_Do_It/practice/src/main/java/study/practice/controller/board/BoardController.java
@@ -6,8 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import study.practice.domain.board.Board;
+import study.practice.repository.board.BoardUpdateDto;
 import study.practice.service.board.BoardServiceImpl;
 
 @Slf4j
@@ -24,4 +28,44 @@ public class BoardController {
         model.addAttribute("boards", boards);
         return "board/boards";
     }
+
+    @GetMapping("/{boardId}")
+    public String board(@PathVariable Long boardId, Model model) {
+        Board board = boardService.findBoardById(boardId).get();
+        model.addAttribute("board", board);
+        return "board/board";
+    }
+
+    @GetMapping("/add")
+    public String addBoard() {
+        return "board/addBoard";
+    }
+
+    @PostMapping("/add")
+    public String createBoard(@ModelAttribute BoardUpdateDto createParam, Model model) {
+        String writer = "임시 작성자"; // 향후 세션에서 받아올 예정
+        Board board = new Board(
+                createParam.getTitle(),
+                createParam.getContent(),
+                writer
+        );
+        boardService.createBoard(board);
+        model.addAttribute("board", board);
+        return "redirect:/boards";
+    }
+
+    @GetMapping("/{boardId}/edit")
+    public String editForm(@PathVariable Long boardId, Model model) {
+        Board board = boardService.findBoardById(boardId).get();
+        model.addAttribute("board", board);
+        return "board/editBoard";
+    }
+
+    @PostMapping("/{boardId}/edit")
+    public String editBoard(@ModelAttribute BoardUpdateDto updateParam, @PathVariable Long boardId) {
+        boardService.updateBoard(boardId, updateParam);
+        return "redirect:/boards/{boardId}";
+    }
+
+
 }
