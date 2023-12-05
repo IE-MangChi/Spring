dd2cca5f8b2204ff6407850e16e267c914492fe3
IE-MangChi <hsm983278@gmail.com>
2023-12-05 18:53:31 +0900

    feat: 메모리 저장소 기능 추가
    -create: 게시물 생성
    -update: 게시물 수정
    -findAll: 게시물 전체 조회
    -findById: 게시물 단건 조회
    -delete: 게시물 삭제

---
+++ Lets_Do_It/practice/src/main/java/study/practice/repository/board/BoardRepository.java
@@ -0,0 +1,20 @@
+package study.practice.repository.board;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import study.practice.domain.board.Board;
+
+@Repository
+public interface BoardRepository {
+    // 게시물 등록
+    Board create(Board board);
+    // 게시물 수정
+    void update(Long id, BoardUpdateDto updateParam);
+    // 단건 게시물 조회
+    Optional<Board> findById(Long id);
+    // 다중 게시물 조회
+    List<Board> findAll();
+    // 게시물 삭제
+    void delete(Long id);
+}
---
+++ Lets_Do_It/practice/src/main/java/study/practice/repository/board/MemoryBoardRepository.java
@@ -0,0 +1,44 @@
+package study.practice.repository.board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import study.practice.domain.board.Board;
+
+public class MemoryBoardRepository implements BoardRepository{
+
+    private static final Map<Long, Board> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Board create(Board board) {
+        board.setId(sequence++);
+        store.put(board.getId(), board);
+        return board;
+    }
+
+    @Override
+    public void update(Long id, BoardUpdateDto updateParam) {
+        Board findBoard = store.get(id);
+        findBoard.setTitle(updateParam.getTitle());
+        findBoard.setContent(updateParam.getContent());
+    }
+
+    @Override
+    public Optional<Board> findById(Long id) {
+        Board findBoard = store.get(id);
+        return Optional.ofNullable(findBoard);
+    }
+
+    @Override
+    public List<Board> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public void delete(Long id) {
+        store.remove(id);
+    }
+}
